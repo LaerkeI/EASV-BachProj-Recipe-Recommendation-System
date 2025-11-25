87e9108d9708faa1c8697b889df4858482c48635
LaerkeI <imeland3@gmail.com>
2025-11-25 21:18:12 +0100

    Added RecipeService and interface

---
+++ RecipeManagementService/Application/Services/RecipeService.cs
@@ -0,0 +1,40 @@
+﻿using RecipeManagementService.Domain.Entities;
+using RecipeManagementService.Domain.Interfaces;
+
+namespace RecipeManagementService.Application.Services
+{
+    public class RecipeService : IRecipeService
+    {
+        private readonly IRecipeRepository _recipeRepository;
+
+        public RecipeService(IRecipeRepository recipeRepository)
+        {
+            _recipeRepository = recipeRepository;
+        }
+
+        public async Task<List<Recipe>> GetAllRecipesAsync()
+        {
+            return await _recipeRepository.GetAllRecipesAsync();
+        }
+
+        public async Task<Recipe?> GetRecipeByIdAsync(string recipeId)
+        {
+            return await _recipeRepository.GetRecipeByIdAsync(recipeId);
+        }
+
+        public async Task CreateRecipeAsync(Recipe recipe)
+        {
+            await _recipeRepository.CreateRecipeAsync(recipe);
+        }
+
+        public async Task<bool> UpdateRecipeAsync(string recipeId, Recipe updatedRecipe)
+        {
+            return await _recipeRepository.UpdateRecipeAsync(recipeId, updatedRecipe);
+        }
+
+        public async Task<bool> DeleteRecipeAsync(string recipeId)
+        {
+            return await _recipeRepository.DeleteRecipeAsync(recipeId);
+        }
+    }
+}
---
+++ RecipeManagementService/Domain/Interfaces/IRecipeService.cs
@@ -0,0 +1,13 @@
+﻿using RecipeManagementService.Domain.Entities;
+
+namespace RecipeManagementService.Domain.Interfaces
+{
+    public interface IRecipeService
+    {
+        Task<List<Recipe>> GetAllRecipesAsync();
+        Task<Recipe?> GetRecipeByIdAsync(string recipeId);
+        Task CreateRecipeAsync(Recipe recipe);
+        Task<bool> UpdateRecipeAsync(string recipeId, Recipe updatedRecipe);
+        Task<bool> DeleteRecipeAsync(string recipeId);
+    }
+}
